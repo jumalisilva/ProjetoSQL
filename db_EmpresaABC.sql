680bb13211a9a1988dd8e57d5953943a1193a069
jumalisilva <juliamalisilva@gmail.com>
2023-08-08 17:15:01 -0300

    UC10

--- db_EmpresaABC.sql
+++ db_EmpresaABC.sql
@@ -50,3 +50,19 @@ select * from produtos where nome not like '%Y%';
 -- 6. Exibir todos os produtos que se iniciem com nome 'MO' e tenham como tipo as letras 
 -- 'MA'.
 select * from produtos where nome like 'MO%' and tipo like '%MA%';
+
+-- Segundo Laboratório
+
+-- 1. Escreva uma consulta que exiba os dados da tabela PRODUTOS na forma crescente 
+-- pelo campo NOME. 
+select * from produtos order by nome asc;
+
+-- 2. Escreva uma consulta que exiba o NOME dos produtos ordenado de forma decrescente 
+-- e o VALOR de forma crescente. 
+select * from produtos order by name asc, valor desc;
+
+-- 3. Escreva uma consulta que exiba os três produtos mais caros. 
+select * from produtos order by valor desc limit 3;
+
+-- 4. Escreva uma consulta que exiba o VALOR do produto mais barato. 
+select * from produtos order by valor asc limit 1;
